855f1d0ac24eff94c928593238bbbc37150c40b2
AkramGafurov <akramminsk@yander.ru>
2021-02-10 00:30:57 +0300

    feat: set functional of catalog

--- catalog/components/catalog/catalog.js
+++ catalog/components/catalog/catalog.js
@@ -1,15 +1,44 @@
 class Products {
+  constructor(){
+    this.lableAdd = 'Добавить в корзину';
+    this.lableRemove = 'Удалить из корзины';
+    this.classActive = 'products-item__button_active'
+  }
+
+  changeLocalStorage(id, element){
+  let {isAdded,currentLocalStorage} = localStorageUtil.setLocalStorage(id);
+  if (isAdded){
+    element.classList.add(this.classActive);
+    element.innerHTML =  this.lableRemove;
+  }else{
+    element.classList.remove(this.classActive);
+    element.innerHTML = this.lableAdd;
+  }
+  }
 
   render(){
-  let catalogItems = '';
-    CATALOG.forEach(({img, name, price, button})=>
-  {  let catalogItem =
-     `<li class = products-item>
-     <p class = products-item__name>${name}</p>
+    
+    let buttonText = '';
+    let buttonClass = '';
+    let catalogItems = '';
+    let catalogStore = localStorageUtil.getLocalStorage();
 
-             <img class = catalog__img src = ${img} alt ='guitar'>
-            <p class = products-item__price>Price: ${price.toLocaleString()} rub</p>
-            <button class = products-item__button>${button}</button>
+    CATALOG.forEach(({id,img, name, price, button})=>{
+      
+    if(catalogStore.indexOf(id) === -1){
+      buttonClass = ''
+      buttonText = this.lableAdd
+    }else{
+      buttonText =  this.lableRemove;
+      buttonClass = this.classActive
+    }
+
+    let catalogItem =
+     `<li class = products-item>
+        <p class = products-item__name>${name}</p>
+        <img class = catalog__img src = ${img} alt ='guitar'>
+        <p class = products-item__price>Price: ${price.toLocaleString()} rub</p>
+        <button onclick = "products.changeLocalStorage('${id}', this)" class = 'products-item__button ${buttonClass}'>${buttonText}</button>
       </li>`
       catalogItems += catalogItem;
     }
